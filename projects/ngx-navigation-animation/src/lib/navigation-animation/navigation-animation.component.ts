9178c892c3d3e458a077be41f58dee209a5fa7ff
Zenith Wogwugwu <uzenith360@gmail.com>
2023-01-28 02:02:33 +0100

    fix: update the app-navigation-animation to ngx-navigation-animation component selector

--- projects/ngx-navigation-animation/src/lib/navigation-animation/navigation-animation.component.ts
+++ projects/ngx-navigation-animation/src/lib/navigation-animation/navigation-animation.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-navigation-animation',
+  selector: 'ngx-navigation-animation',
   templateUrl: './navigation-animation.component.html',
   styleUrls: ['./navigation-animation.component.css']
 })
